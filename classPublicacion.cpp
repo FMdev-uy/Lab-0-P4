909dce87022cbae643a2a36d7433fbccb7c6410a
SantiagoA245 <tiagosanmvd@gmail.com>
2025-03-18 21:58:15 -0300

    claseCorregida

--- classPublicacion.cpp
+++ classPublicacion.cpp
@@ -1,56 +1,28 @@
-<<<<<<< Updated upstream
-#include <dataTypes.cpp>
-
-class Publicacion
-{
-=======
 #include <iostream>
 #include <string>
 #include <dataTypes.cpp>
+
 using namespace std;
-
-// Definimos estructuras auxiliares (asumimos su existencia)
-struct DTFecha {
-    int dia, mes, anio;
-};
-
-struct DTRefer {
-    string referencia;
-};
 
 // Clase abstracta
 class publicacion {
->>>>>>> Stashed changes
-private:
-    string DOI;
-    string Titulo;
-    DTFecha Fecha;
-    DTRefer DT;
+    private:
+        string DOI;
+        string Titulo;
+        DTFecha Fecha;
+        DTRefer DT;
+    public:
+        // Constructor
+        publicacion(string doi, string titulo, DTFecha fecha, DTRefer dt) 
+            : DOI(doi), Titulo(titulo), Fecha(fecha), DT(dt) {}  // Inicialización de miembros
 
-public:
-<<<<<<< Updated upstream
-    DTRefer get_DT();
-    bool contienePalabra(string);
+        // Destructor
+        virtual ~publicacion() {}
+
+        DTRefer get_DT() {
+            return this->DT;
+        }
+
+        // Método virtual puro
+        virtual bool contienePalabra(string palabra) = 0;
 };
-
-DTRefer Publicacion ::get_DT()
-{
-    return this->DT;
-};
-=======
-    // Constructor
-    publicacion(string doi, string titulo, DTFecha fecha, DTRefer dt) 
-        : DOI(doi), Titulo(titulo), Fecha(fecha), DT(dt) {}  // Inicialización de miembros
-
-    // Destructor
-    virtual ~publicacion() {}
-
-    // Método getter
-    DTRefer get_DT() {
-        return this->DT;
-    }
-
-    // Método virtual puro (hace que la clase sea abstracta)
-    virtual bool contienePalabra(string palabra) = 0;
-};
->>>>>>> Stashed changes
